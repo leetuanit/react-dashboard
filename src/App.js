e20f3fc62fbb815ba1d14765dbd7b031d6932bc0
Lee Tuan <tuanlm1989@gmail.com>
2019-03-23 18:35:24 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
 import React, { Component } from 'react';
 
 import Sidebar from './components/Sidebar';
-import Topbar from './components/Topbar';
-import Footer from './components/Footer';
+//import Topbar from './components/Topbar';
+//import Footer from './components/Footer';
 
 export default class extends Component {
-  constructor() {
-    super();
-    this.state = {
-      toggled: false
-    }
-    this.toggled = this.toggled.bind(this);
-  }
-  toggled() {
-    this.setState({ toggled: !this.state.toggled });
-  }
   render() {
-    let sidebarClassName = this.state.toggled ? "sidebar toggled" : "sidebar";
     return (
       <div className="page-wrapper">
-        <nav id="sidebar" className={sidebarClassName}>
-          <Sidebar />
-        </nav>
-        <div className="main-panel">
-          <Topbar toggled={this.toggled} />
-          <div className="main-content"></div>
-          <Footer />
-        </div>
+        <Sidebar />
       </div>
     );
   }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,58 +2,58 @@ import React, { Component } from 'react';
 import { Nav, NavItem, NavLink } from 'reactstrap';
 
 import Logo from '../logo.svg';
-
 export default class extends Component {
   render() {
     const links = [
       {
-        href: "/index",
-        title: "Index"
-      },
-      {
-        href: "/users",
-        title: "Users"
+        href: "/dashboard",
+        title: "Dashboard",
+        icon: "pe-7s-graph"
       },
       {
         href: "/profile",
-        title: "Profile"
+        title: "User profile",
+        icon: "pe-7s-user"
+      },
+      {
+        href: "/icon",
+        title: "Icon",
+        icon: "pe-7s-science"
       },
       {
         href: "/notification",
-        title: "Notification"
+        title: "Notification",
+        icon: "pe-7s-bell"
       }
     ];
     return (
-      <div className="wrapper">
-        <a href="/">
-          <div className="header">
-            <div className="icon">
-              <img src={Logo} alt="" />
-            </div>
-            <div className="text">
-              DASHBOARD
-            </div>
+      <nav id="sidebar" className="sidebar">
+        <div className="wrapper">
+          <div className="sidebar-background" />
+          <div className="logo">
+            <a href="http://localhost:3000" className="simple-text logo-mini">
+              <div className="logo-img">
+                <img src={Logo} alt="Something" />
+              </div>
+            </a>
+            <a href="http://localhost:3000" className="simple-text logo-normal">Creative Tim</a>
           </div>
-        </a>
-        <div className="content">
-          <Nav vertical>
-            {links.map((link, index) => {
-              return (
-                <NavItem key={index}>
-                  <NavLink href={link.href}>
-                    <div className="icon">
-                      <img src={Logo} alt="" />
-                    </div>
-                    <div className="text">
+          <div className="content">
+            <Nav vertical>
+              {links.map((link, index) => {
+                return (
+                  <NavItem key={index}>
+                    <NavLink href={link.href}>
+                      <i className={link.icon} />
                       {link.title}
-                    </div>
-                  </NavLink>
-                </NavItem>
-              );
-            })}
-          </Nav>
+                    </NavLink>
+                  </NavItem>
+                );
+              })}
+            </Nav>
+          </div>
         </div>
-      </div>
+      </nav>
     );
   }
 }
