6ade73a781e36bee084561af269176824c71048e
Lee Tuan <tuanlm1989@gmail.com>
2019-03-24 20:56:56 +0700

    Update build

---
+++ build/precache-manifest.3d4c811ae1361425dd3b3797ec21a6d5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "97e56fb709af4d37006a",
+    "url": "/static/css/main.0abfe0b6.chunk.css"
+  },
+  {
+    "revision": "97e56fb709af4d37006a",
+    "url": "/static/js/main.f1d04431.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "62de2e38c4375537d8ec",
+    "url": "/static/js/2.afb86211.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "0c084afaedf724b349f112d739611e5d",
+    "url": "/index.html"
+  }
+];
